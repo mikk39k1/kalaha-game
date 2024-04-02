7c9ae62a3dd0b96b622fb7e7f32772dc57a033f8
realkoder <alex8970@stud.kea.dk>
2024-04-02 09:05:22 +0200

    Deleted comments

--- src/components/KalahaGame.tsx
+++ src/components/KalahaGame.tsx
@@ -54,40 +54,6 @@ const KalahaGame = () => {
             )
           })}
         </div>
-
-        {/* Spiller 2's Kalaha pit på venstre side (Blå) */}
-        {/* <div className="w-24 h-48 md:w-32 md:h-64 bg-blue-200 border border-gray-400 rounded-lg flex items-center justify-center cursor-pointer m-1" onClick={() => handlePitClick(13)}>
-          {pits[14].seeds}
-        </div> */}
-
-        {/* <div className='flex flex-col items-center'> */}
-
-        {/* Spiller 2's pits på øverste række */}
-        {/* <div className="flex flex-row-reverse">
-            {pits.slice(8, 14).map((pit, index) => (
-              <div key={index} className="w-16 h-16 md:w-24 md:h-24 bg-gray-200 border border-gray-400 rounded-full flex items-center justify-center cursor-pointer m-1" onClick={() => handlePitClick(index + 8)}>
-                {pit.seeds}
-              </div>
-            ))}
-          </div> */}
-
-
-        {/* <div className="w-4 h-4 mx-6"></div> */}
-
-        {/* Spiller 1's pits På nederste række */}
-        {/* <div className="flex flex-row">
-            {pits.slice(1, 7).map((pit, index) => (
-              <div key={index} className="w-16 h-16 md:w-24 md:h-24 bg-gray-200 border border-gray-400 rounded-full flex items-center justify-center cursor-pointer m-1" onClick={() => handlePitClick(index + 1)}>
-                {pit.seeds}
-              </div>
-            ))}
-          </div> */}
-
-        {/* </div> */}
-        {/* Spiller 1's Kalaha pit til højre (Rød) */}
-        {/* <div className="w-24 h-48 md:w-32 md:h-64 bg-red-200 border border-gray-400 rounded-lg flex items-center justify-center cursor-pointer m-1" onClick={() => handlePitClick(7)}>
-          {pits[7].seeds}
-        </div> */}
       </div>
     </div>
   );
